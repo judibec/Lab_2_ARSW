fdc704c67a5f06bb72cb31ad2f9d31cded87ce9c
unknown <juan.becerra-pe@labinfo.is.escuelaing.edu.co>
2022-08-18 09:57:14 -0500

    firt commit

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,13 +1,35 @@
 package edu.eci.arsw.primefinder;
 
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
+		PrimeFinderThread pft=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread pft1=new PrimeFinderThread(10000000, 20000000);
+		PrimeFinderThread pft2=new PrimeFinderThread(20000000, 30000000);
+
+
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("hola");
+			}
+		}, 0, 5000);
+		timer.cancel();
+
+//		LocalDateTime date = LocalDateTime.now();
+//		int seconds = date.toLocalTime().toSecondOfDay();
+//		if (seconds == 5000){
+//			System.out.println("hola");
+//		}
+//		pft.start();
+//		pft1.start();
+//		pft2.start();
 		
 	}
 	
